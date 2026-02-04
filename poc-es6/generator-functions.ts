8ba3529c92f357ce89b8a86507451509363d4ca2
Henrique Eidt <henriqueidt@gmail.com>
2026-02-04 10:42:11 -0300

    add generator case

--- poc-es6/generator-functions.ts
+++ poc-es6/generator-functions.ts
@@ -12,7 +12,7 @@
 
 function* idMaker() {
   let index = 0;
-  while (true) {
+  while (index < 3) {
     yield index++;
   }
 }
@@ -22,4 +22,37 @@ const idGenerator = idMaker();
 // when the generator `next()` is called, it will execute its body,
 // until the next `yield`, `yield*` `return` expression or if an error is thrown
 
+/**
+ * In this case, the `next()` call will return an object:
+ * {
+ *  value: 0, - the value yielded by the function
+ *  done: false - will only return true if the function is finished (return or error)
+ * }
+ * */
 console.log(idGenerator.next()); // 0
+console.log(idGenerator.next()); // 1
+console.log(idGenerator.next()); // 2
+
+/**
+ * For this last call, as the function gets out of the while loop
+ * and there is no more yieldede values, it returns
+ * {
+ *  value: undefined, - no yielded value
+ *  done: true - the function has "finished"
+ * }
+ * */
+console.log(idGenerator.next());
+
+// Passing parameters to generator functions
+function* logValue(): Generator<undefined, void, string> {
+  console.log(1, yield);
+  console.log(2, yield);
+  console.log(3, yield);
+}
+
+const generatorLog = logValue();
+
+generatorLog.next();
+generatorLog.next("blue"); // 1 blue
+generatorLog.next("yellow"); // 2 yellow
+generatorLog.next("green"); // 3 green
